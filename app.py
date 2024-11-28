65c998d78c226958728517b82ee844b2039dc08e
Luca Cisotto <luca.cisotto@sourcesense.com>
2024-11-28 16:38:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ def hello():
 
 @app.route('/healthz')
 def healthz():
-    return "OK", 200  # Risposta per la liveness probe
+    return "OK", 200
+
+@app.route('/readyz')
+def readyz():
+    return "Ready", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
