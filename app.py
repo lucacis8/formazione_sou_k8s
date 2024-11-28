7c39328f0ccc24714c221200f298a81ed2c43dd7
Luca Cisotto <luca.cisotto@sourcesense.com>
2024-11-28 16:28:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
+@app.route('/healthz')
+def healthz():
+    return "OK", 200  # Risposta per la liveness probe
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
